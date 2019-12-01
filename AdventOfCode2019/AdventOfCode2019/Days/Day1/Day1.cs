965c5e69f763797f90e85edfcbc9531889121258
Alexander Wood <awoodsey@me.com>
2019-12-01 10:45:34 +0000

    Add AdventOfCode2019 Project & Day1

---
+++ AdventOfCode2019/AdventOfCode2019/Days/Day1/Day1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Days.Day1
+{
+    class Day1
+    {
+        public void ExecuteDay1Part1()
+        {
+            Console.WriteLine("Part 1");
+            var results = new List<int>();
+
+            using (var streamReader = new StreamReader(@"C:\Users\Alexander\Documents\adventofcode2019\AdventOfCode2019\AdventOfCode2019\Days\Day1\day1input.txt"))
+            {
+                while(!streamReader.EndOfStream)
+                {
+                    var input = decimal.Parse(streamReader.ReadLine());
+
+                    results.Add((int)CalculateFuel(input));
+
+                    //Console.WriteLine(input);
+                }
+            }
+
+            Console.WriteLine(results.Sum());
+        }
+
+        public void ExecuteDay1Part2()
+        {
+            Console.WriteLine("Part 2");
+
+            var results = new List<int>();
+
+            using (var streamReader = new StreamReader(@"C:\Users\Alexander\Documents\adventofcode2019\AdventOfCode2019\AdventOfCode2019\Days\Day1\day1input.txt"))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var fuelResults = new List<int>();
+                    var input = decimal.Parse(streamReader.ReadLine());
+
+                    fuelResults.Add((int)CalculateFuel(input));
+
+                    var hasFuelRemaining = true;
+                    var currentFuelValue = input;
+
+                    while (hasFuelRemaining)
+                    {
+                        currentFuelValue = CalculateFuel(currentFuelValue);
+
+                        if (currentFuelValue > 0)
+                        {
+                            fuelResults.Add((int)currentFuelValue);
+
+                            continue;
+                        }
+
+                        hasFuelRemaining = false;
+                    }
+
+                    var totalFuel = fuelResults.Sum();
+
+                    results.Add(totalFuel);
+
+                    //Console.WriteLine(totalFuel);
+                }
+            }
+
+            Console.WriteLine(results.Sum());
+        }
+
+        private decimal CalculateFuel(decimal fuel)
+        {
+            var result = Math.Floor(fuel / 3);
+
+            result -= 2;
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2019/AdventOfCode2019/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Execute())
+            {
+
+            }
+        }
+
+        private static bool Execute()
+        {
+            Console.WriteLine("Enter which day to execute ...");
+
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out var intInput))
+            {
+                Console.WriteLine("Enter a number");
+                return false;
+            }
+
+            switch (intInput)
+            {
+                case 1:
+                    var day1 = new Days.Day1.Day1();
+
+                    day1.ExecuteDay1Part1();
+                    day1.ExecuteDay1Part2();
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
